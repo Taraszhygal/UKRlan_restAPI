462cd4c3c60c947be9145b8a16da96e8c2b707c1
codeflow_nst <zhygalt@gmail.com>
2023-04-01 14:40:13 +0300

    Update Setup.sh script

--- setup.sh
+++ setup.sh
@@ -1,14 +1,12 @@
 #!bin/bash
 apt update && apt install git nodejs npm postgresql postgresql-contrib
 systemctl start postgresql.service
-
-
-sudo -i -u postgres && psql -U postgres -tc "SELECT 1 FROM pg_database WHERE datname = 'diploma'" | grep -q 1 || psql -U postgres -c "CREATE DATABASE diploma"
-
-psql -U postgres -tc "CREATE TABLE data ("_id integer, add_time VARCHAR(50),carrier integer, city VARCHAR(50), expired_time VARCHAR(50), given_time VARCHAR(50),manufacturer integer, order_number VARCHAR(50),state VARCHAR(50), sync_time VARCHAR(50), technology integer")"
-
-psql -U postgres -tc "COPY persons(first_name, last_name, dob, email) FROM './ruslan-duda.csv' DELIMITER ',' CSV HEADER;"
-
+psql -U postgres -c "CREATE DATABASE diploma" 
+psql -U postgres -c "\c diploma" -tc "CREATE TABLE data (_id integer, add_time VARCHAR(50),carrier integer, city VARCHAR(50), expired_time VARCHAR(50), given_time VARCHAR(50),manufacturer integer, order_number VARCHAR(50),state VARCHAR(50), sync_time VARCHAR(50), technology integer)" -c "\dt"
+psql -U postgres -c "\c diploma" -tc "\COPY persons(first_name, last_name, dob, email) FROM './ruslan-duda.csv' DELIMITER ',' CSV HEADER;"
 npm i
-
-node index.js
+npm install -g forever
+forever start ./index.js 
+wget https://bin.equinox.io/c/bNyj1mQVY4c/ngrok-v3-stable-linux-amd64.tgz && tar -xvzf ngrok-v3-stable-linux-amd64.tgz
+./ngrok config add-authtoken $1
+./ngrok https 9393 > /dev/null &
